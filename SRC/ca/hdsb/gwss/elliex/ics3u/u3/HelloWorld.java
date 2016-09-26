ab85bd587a0b99cc79e27e8821021f06476575be
1xuell <1xuell@GWS03903.ihdsb.CA>
2016-09-26 14:46:26 -0400

    added alstanding and smile to the package

---
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u3/HelloWorld.java
@@ -0,0 +1,26 @@
+package ca.hdsb.gwss.elliex.ics3u.u3;
+
+/*
+ * Name:
+ * Date:
+ * Version: (what version are you working on?) final version 1.0
+ * Description: This program displays hello world to the console
+ */
+
+/**
+ *
+ * @author 1xuell
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //OUTPUT TO SCREEN
+        System.out.println("\"Hello World\"");
+        System.err.println("dfkjdsklfj");
+    }
+
+}
---
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u3/smile.java
@@ -0,0 +1,21 @@
+/*
+ * Ellie
+ * Sept 26, 2016
+ * and open the template in the editor.
+ */
+package ca.hdsb.gwss.elliex.ics3u.u3;
+
+/**
+ *
+ * @author 1xuell
+ */
+public class smile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
