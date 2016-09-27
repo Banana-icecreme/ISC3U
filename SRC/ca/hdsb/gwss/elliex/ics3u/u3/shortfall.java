a483cfc762aae7b30c3e31849530a24984ad9ddb
1xuell <1xuell@GWS03903.ihdsb.CA>
2016-09-27 14:46:36 -0400

    added shortfall

---
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u3/shortfall.java
@@ -0,0 +1,49 @@
+/*
+ * Ellie
+ * Sept. 27,2016
+ * version 1.0
+ * ShortFall; Double Jeopardy; Character Assassination; Payroll; 
+ */
+package ca.hdsb.gwss.elliex.ics3u.u3;
+
+/**
+ *
+ * @author 1xuell
+ */
+public class shortfall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//
+//        //ShortFall
+//        int value = 3556656;
+//        System.out.println("A short: " + value);
+//        
+//        //Double Jeopardy
+//        double value = 3211022352323230*130310130312031030130;
+//        System.out.println("A double: " + value);
+//
+//        //Character Assassination
+//        char s = 'a';
+//        System.out.println("A char: " + s);
+//
+//        //Payroll
+//        long hoursWorked = 20;
+//        double payRate = 10.0, taxRate = 0.10;
+//        double payAmount = hoursWorked * payRate;
+//        double taxAmount = hoursWorked * payRate * taxRate;
+//        
+//        System.out.println("Hours Worked: " + hoursWorked);
+//        System.out.println("pay Amount  : " + payAmount);
+//        System.out.println("tax Amount  : " + taxAmount);
+
+        //Value of a Quadratic
+        double X = 4.0;
+        double answer = 3*X*X - 8*X + 4;
+        
+        System.out.println("At x" + " = " + X + " the value is " + answer);
+    }
+
+}
--- SRC/ca/hdsb/gwss/elliex/ics3u/u3/smile.java
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u3/smile.java
@@ -15,7 +15,8 @@ public class smile {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double value = 3556656.244;
+        System.out.println("A short: " + value);
     }
-    
+
 }
