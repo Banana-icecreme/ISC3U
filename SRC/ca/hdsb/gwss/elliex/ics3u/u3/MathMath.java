dce494d9c4bae22ca00b8a722565a1d0c2c3d0ed
1xuell <1xuell@GWS03903.ihdsb.CA>
2016-09-29 14:45:38 -0400

    updated mathmath, and added questions and answers

--- SRC/ca/hdsb/gwss/elliex/ics3u/u3/MathMath.java
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u3/MathMath.java
@@ -6,6 +6,9 @@
  */
 package ca.hdsb.gwss.elliex.ics3u.u3;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 /**
  *
  * @author 1xuell
@@ -48,7 +51,7 @@ public class MathMath {
         
         
         //Restaurant Tab
-        double food = 200;
+        double food = 201;
         double tip = food * 0.1;
         double tax = food * 0.13;
         double total = tax + tip + food;
@@ -61,7 +64,7 @@ public class MathMath {
         System.out.println("Total: " + total);
         
         //Change
-        int change = 555; 
+        int change = 1234; 
         int quarters = change/25;
         int Q = Math.round(quarters);
         int dimes = (change-Q*25)/10;
@@ -77,13 +80,28 @@ public class MathMath {
         System.out.println("Dimes: "+D);
         System.out.println("Nickels: " + N);
         System.out.println("Cents: " + C);
-                
-                
-                
-                
-                
-              
-                
+     
+        //Digits
+        int number = 453;
+        int hundredth = number/100;
+        int hundredth2 = Math.round (hundredth);
+        int tens = number - (hundredth2 * 100);
+        int tens2 = tens/10;
+        int tens3 = Math.round (tens2);
+        int ones = number - (hundredth2 * 100) - (tens3 * 10);
+        
+        System.out.println(" ");
+        System.out.println("3 digit number: " + number);
+        System.out.println("Hundredth: " + hundredth2);
+        System.out.println("Tens: " + tens3);
+        System.out.println("Ones: " + ones);
+        
+        // Heron's Formula
+        int side1 = 2;
+        int side2 = 2;
+        int side3 = 2;
+        int s = (side1 + side2 + side3)/2;
+        double area = Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
     }
     
 }
