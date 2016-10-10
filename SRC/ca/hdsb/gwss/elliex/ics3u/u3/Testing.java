f7d9d7f4a29ad340a51a1b44bbe689ef7de21ed1
elli <elli@192.168.2.12>
2016-10-10 16:26:04 -0400

    Added the Java main class called "Testing".

---
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u3/Testing.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.hdsb.gwss.elliex.ics3u.u3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author elli
+ */
+public class Testing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String response; 
+    Scanner input = new Scanner( System.in );
+    System.out.println("hi");
+    response = input.nextLine();
+    System.out.println(response);
+    }
+    
+}
