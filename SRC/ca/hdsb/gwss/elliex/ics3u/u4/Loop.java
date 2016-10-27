3c053928c8ea1c105354942e01dc10a2d4c212be
elli <elli@192.168.2.12>
2016-10-26 20:06:51 -0400

    Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooop .java is finished.

--- SRC/ca/hdsb/gwss/elliex/ics3u/u4/Loop.java
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u4/Loop.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ellie T. Xu
+ * October something of 2016. Nvm, it's the 26th
+ * Version numero une point une.
+ * A system to repeat a mathmatical action over and over again. Aka a math loop. 
  */
 package ca.hdsb.gwss.elliex.ics3u.u4;
 
@@ -18,26 +19,37 @@ public class Loop {
      */
     public static void main(String[] args) {
         // loop
-        int a, b, c, d;
+        int a, b, c, d, d2;
         String e;
-        
+
         Scanner input = new Scanner(System.in);
         System.out.println("Start number");
-        a = Integer.parseInt( input.nextLine() );
+        a = Integer.parseInt(input.nextLine());
         System.out.println("The loop is bigger and equal to: ");
-        b = Integer.parseInt( input.nextLine() );
+        b = Integer.parseInt(input.nextLine());
         System.out.println("The loop is smaller and equal to: ");
-        c = Integer.parseInt( input.nextLine() );
+        c = Integer.parseInt(input.nextLine());
         System.out.println("Value for the assignment statement:");
-        d = Integer.parseInt( input.nextLine() );
+        d = Integer.parseInt(input.nextLine());
         System.out.println("Assignment Statment:");
         e = input.nextLine();
-        
-        if (e.equals(+) "+" || e == "-") {
-            if (e == "-") {
-                int d2 = d * -1;
+
+        if (e.equals("+") || e.equals("-")) {
+            if (e.equals("-")) {
+                d2 = d * -1;
+            } else {
+                d2 = d;
             }
-            for (; a >= b && a <= c; a += d) {
+            for (; a >= b && a <= c; a += d2) {
+                System.out.println(a);
+            }
+        } else if (e.equals("/")) {
+            for (; a >= b && a <= c; a /= d) {
+                System.out.println(a);
+                //how do i stop the loop once a certain number has been repeated multiple times?
+            }
+        } else if (e.equals("*")) {
+            for (; a >= b && a <= c; a *= d) {
                 System.out.println(a);
             }
         }
