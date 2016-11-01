abe0f287780e4781ca7700dd795e728f356cb529
1xuell <1xuell@GWS03903.ihdsb.CA>
2016-11-01 13:25:35 -0400

    RepetitonLoop working on it

--- SRC/ca/hdsb/gwss/elliex/ics3u/u4/RepetitonLoop.java
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u4/RepetitonLoop.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ellie Xu 
+ * first day of november 2016.
+ * the one and only version of this program. Aka version 1.0
+ * Reptition loop assignment of for and while loops. 
  */
 package ca.hdsb.gwss.elliex.ics3u.u4;
 
@@ -20,17 +21,19 @@ public class RepetitonLoop {
     public static void main(String[] args) {
         //variables
         int program;
-        int over;
+        int over = 0;
 
         //objects 
         Scanner input = new Scanner(System.in);
 
         do {
 
-            System.out.println("Type 1 for Ounce & Grams \nType 2 for Compund Interest\n"
-                    + "");
-            program = input.nextInt();
+            System.out.println("Type 1 for Ounce & Grams \nType 2 for Compund Interest\nType 3 for "
+                    + "Closer to Two\n"
+                    + "Type 4 for the Fibonacci Sequence\n"
+                    + "Type 5 for Lab Animals\n");
 
+            program = input.nextInt();
             switch (program) {
                 case 1:
                     ouncesToGrams();
@@ -38,12 +41,22 @@ public class RepetitonLoop {
                 case 2:
                     compoundInterest();
                     break;
+                case 3:
+                    CloserToTwo();
+                    break;
+                case 4:
+                    FibonacciSequence();
+                    break;
+                case 5:
+                    LabAnimals();
+                    break;
+                default:
+                    System.out.println("There is no such program.");
             }
-            System.out.println("\nWould you like to start over?\nType 1 for yes.\nType any other number to exit.");
-            over = input.nextInt();
+//            System.out.println("\nWould you like to start over?\nType 1 for yes.\nType any other number to exit.");
+//            over = input.nextInt();
 
         } while (over == 1);
-
     }
 
     public static void ouncesToGrams() {
@@ -62,22 +75,75 @@ public class RepetitonLoop {
         int year;
         double interest;
         double invest;
-        NumberFormat number = NumberFormat.getIntegerInstance();
+        NumberFormat number = NumberFormat.getCurrencyInstance();
 
-        System.out.println("How many years?");
+        System.out.print("How many years? Max of 15 years.");
         year = input.nextInt();
-        System.out.println("How much money invested?");
+        System.out.print("How much money invested?");
         invest = input.nextDouble();
-        System.out.println("What is the interest per year in percentage.");
+        System.out.print("What is the interest per year in percentage?");
         interest = input.nextDouble();
 
-        System.out.format("%5s %10s %10s %10s \n", "Year", "$ Invested", "Interest", "Total");
-        for (int m = 1; m <= year; m++) {
-            double interest1 = invest * (interest / 100);
-            double total = interest1 + invest;
-            System.out.format("%5s %10s %10s %10s \n", m, number.format(invest), number.format(interest1), number.format(total));
-            invest = total;
+        if (year >= 0 && year <= 15) {
+            System.out.format("%5s %15s %15s %15s \n", "Year", "$ Invested", "Interest", "Total");
+            for (int m = 1; m <= year; m++) {
+                double interest1 = invest * (interest / 100);
+                double total = interest1 + invest;
+                System.out.format("%5s %10s %10s %10s \n", m, number.format(invest), number.format(interest1), number.format(total));
+                invest = total;
+            }
+        } else {
+            System.out.println("BAD DATA");
         }
     }
 
+    public static void CloserToTwo() {
+        double a = 2;
+        double b = 0;
+        double c;
+        System.out.println(" ");
+
+        while (b < 2) {
+            a /= 2;
+            System.out.print(b + "+" + a + " = ");
+            b += a;
+            System.out.print(b + "\n");
+
+        }
+    }
+
+    public static void FibonacciSequence() {
+        //vari 
+        int a = 1;
+        int b = 0;
+        int c;
+
+        System.out.print("\n");
+        for (int term = 1; term <= 18; term++) {
+            c = a + b;
+            System.out.println(c);
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void LabAnimals() {
+        //vari
+        int food;
+        int population;
+        int hour = 0;
+        
+        //objects
+        Scanner input = new Scanner (System.in);
+        System.out.print("Intial supply of food: ");
+        food = input.nextInt();
+        System.out.print("Intial population: ");
+        population = input.nextInt();
+
+        for (;food >= population; food += (4000-population)) {
+            System.out.println(hour +""+ population +""+ food);
+            population *= 2;
+            hour ++;
+        }
+    }
 }
