884434206e3f7b414c5929da2f816f320c7b29c9
elli <elli@192.168.2.12>
2016-11-13 15:06:53 -0500

    working on StringFuction.java.

--- SRC/ca/hdsb/gwss/elliex/ics3u/u5/StringFunctions.java
+++ SRC/ca/hdsb/gwss/elliex/ics3u/u5/StringFunctions.java
@@ -20,7 +20,7 @@ public class StringFunctions {
         // TODO code application logic here
         int count;
         Scanner input = new Scanner(System.in);
-        System.out.print("1 for counting vowels.\n2 for something else\n");
+        System.out.print("1 for counting vowels.\n2 for word square.\n3 for compass.\n4 for palindrome. \n");
         count = input.nextInt();
 
         switch (count) {
@@ -28,10 +28,16 @@ public class StringFunctions {
                 CountingVowels();
                 break;
             case 2:
-                WordSpace();
+                WordSquare();
+                break;
+            case 3:
+                Compass();
+                break;
+            case 4:
+                Palindrome();
                 break;
             default:
-                System.out.println("There is no such program yet.");
+                System.out.println("There is no such program.");
         }
     }
 
@@ -41,37 +47,115 @@ public class StringFunctions {
         String vowel = input.nextLine();
         int count = 0;
         for (int i = 0; i < vowel.length(); i++) {
-            switch (vowel.charAt(i)){
+            switch (vowel.charAt(i)) {
                 case 'e':
-                count++;
-                break;
+                    count++;
+                    break;
                 case 'a':
-                count++;
-                break;
+                    count++;
+                    break;
                 case 'i':
-                count++;
-                break;
+                    count++;
+                    break;
                 case 'o':
-                count++;
-                break;
+                    count++;
+                    break;
                 case 'u':
-                count++;
-                break;
+                    count++;
+                    break;
                 case 'y':
-                count++; 
-                break;
-        }
+                    count++;
+                    break;
+            }
         }
         System.out.println(count);
     }
 
-    public static void WordSpace() {
+    public static void WordSquare() {
+        int i = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("\nType in a sentence.");
-        String sen = input.nextLine();
-        for (int i = 0; i < sen.length(); i++) {
-            System.out.println(sen.charAt(i));
+        String sentence = input.nextLine();
+        System.out.println("");
+        while (i <= sentence.length()) {
+            String rest = sentence.substring(1);
+            //System.out.println(rest + sentence.charAt(0));
+            System.out.println(sentence);
+            sentence = rest + sentence.charAt(0);
+            i++;
         }
-
     }
+
+    public static void Compass() {
+        String com, direction = null, face = null;
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nType in the compass direction.");
+        com = input.nextLine();
+        System.out.println(" ");
+
+        switch (com.charAt(0)) {
+            case 's':
+            case 'S':
+                face = "South";
+                break;
+            case 'n':
+            case 'N':
+                face = "North";
+                break;
+            case 'w':
+            case 'W':
+                face = "West";
+                break;
+            case 'e':
+            case 'E':
+                face = "East";
+                break;
+            default:
+                System.out.println("You have a typo.");
+        }
+        switch (com.charAt(3)) {
+            case 's':
+            case 'S':
+                direction = "South";
+                break;
+            case 'n':
+            case 'N':
+                direction = "North";
+                break;
+            case 'w':
+            case 'W':
+                direction = "West";
+                break;
+            case 'e':
+            case 'E':
+                direction = "East";
+                break;
+            default:
+                System.out.println("You have a typo.");
+        }
+        System.out.println("Start facing " + face + ". Turn " + com.substring(1, 3) + " degrees towards the " + direction + ".");
+    }
+
+    public static void Palindrome() {
+        String pal, check = null;
+        int i;
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nType in the word.");
+        pal = input.nextLine();
+        System.out.println(" ");
+        pal.toUpperCase();
+        i = pal.length() - 1;
+
+        while (i >= 0) {
+            System.out.print(pal.charAt(i));
+            i--;
+            check = check + pal.charAt(i);
+        }
+        //if (check == pal) {
+        //    System.out.println("/n" + pal + " is a palindrome.");
+        //} else {
+        //    System.out.println("/n" + pal + " is not a palindrome.");
+        //}
+    }
+    
 }
