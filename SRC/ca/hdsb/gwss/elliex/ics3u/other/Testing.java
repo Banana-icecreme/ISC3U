3bdf9559cc00887b01e82d6c5f2670d97156fd65
john <john@192.168.2.32>
2017-04-13 14:41:19 -0400

    more refinements

--- SRC/ca/hdsb/gwss/elliex/ics3u/other/Testing.java
+++ SRC/ca/hdsb/gwss/elliex/ics3u/other/Testing.java
@@ -4,6 +4,7 @@ package ca.hdsb.gwss.elliex.ics3u.other;
 
 import static ca.hdsb.gwss.elliex.ics3u.other.ArrayUtilEllie.bubbleSort;
 import static ca.hdsb.gwss.elliex.ics3u.other.ArrayUtilEllie.shuffleArray;
+import static ca.hdsb.gwss.elliex.ics3u.other.ArrayUtilEllie.toUpperCase;
 import static ca.hdsb.gwss.elliex.ics3u.other.SOPL.sopl;
 
 /**
@@ -16,17 +17,18 @@ public class Testing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String array[] = new String[6];
-        array[0] = "a";
-        array[1] = "b";
-        array[2] = "z";
-        array[3] = "c";
-        array[4] = "d";
-        array[5] = "A";
-        shuffleArray(array);
-        sopl(array);
-        bubbleSort(array, true);
-        sopl(array);
+//        String array[] = new String[6];
+//        array[0] = "a";
+//        array[1] = "b";
+//        array[2] = "z";
+//        array[3] = "c";
+//        array[4] = "d";
+//        array[5] = "A";
+//        shuffleArray(array);
+//        sopl(array);
+//        bubbleSort(array, true);
+//        sopl(array);
+toUpperCase("were the DHS’s actions justified? ");
 
     }
 }
